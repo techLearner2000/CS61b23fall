0ea51bc30893b49cba6f828219b6f075ba7054d7
YanMinMacMaster <miny13@mcmaster.ca>
2023-12-26 00:07:07 +0000

    finish the lab06

--- lab06/src/UnionFind.java
+++ lab06/src/UnionFind.java
@@ -10,25 +10,45 @@ public class UnionFind {
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        this.data = new int[N];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        int size = 1;
+        while (data[v] > 0) {
+            v = data[v];
+        }
+        size = -data[v];
+        return size;
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return data[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        while (data[v1] > 0) {
+            v1 = data[v1];
+        }
+        while (data[v2] > 0) {
+            v2 = data[v2];
+        }
+        if (v1 == v2) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
@@ -36,7 +56,17 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        // exception
+        if (v < 0 || v >= data.length) {
+            throw new IllegalArgumentException("Invalid item: " + v);
+        }
+
+        // Path compression
+        if (data[v] < 0) {
+            return v;
+        }
+        data[v] = find(data[v]);
+        return data[v];
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -46,6 +76,21 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        int root1 = find(v1);
+        int size_v1 = -data[root1];
+        int root2 = find(v2);
+        int size_v2 = -data[root2];
+
+        if (root1 == root2) return;
+
+        if (size_v1 > size_v2) {
+            data[root1] += data[root2];
+            data[root2] = root1;
+        }
+        else {
+            data[root2] += data[root1];
+            data[root1] = root2;
+        }
     }
 
     /**
